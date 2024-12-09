767495eafe23ab4db612dc76c399cec1d33b5266
Alperen <alperenakcakaya@gmail.com>
2024-12-09 10:16:10 -0500

    Updated gameSchema.js

--- app_server/models/gameSchema.js
+++ app_server/models/gameSchema.js
@@ -4,11 +4,12 @@ const gameSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
-    genres: { type: [String], default: [] }, // Default to an empty array // Array of genres
+    genres: { type: [String], default: [] }, // Array of genres
     platform: [String],
     releaseDate: Date,
     developer: String,
     rating: Number,
+    lastUpdated: { type: String, default: 'Not available' }, // Default value for lastUpdated
 });
 
 module.exports = mongoose.model('Game', gameSchema);
