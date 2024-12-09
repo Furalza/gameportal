5287e3a253b2cf02f7125d1e827351ea32e2c4b3
Alperen <49041630+Furalza@users.noreply.github.com>
2024-12-09 10:03:00 -0500

    Update db.js

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+
+// Corrected MongoDB URI
+const dbURI = process.env.MONGODB_URI;
+
+// Use native promises
+mongoose.Promise = global.Promise;
+
+// Connect to MongoDB
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Mongoose is connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
+
+module.exports = mongoose;
 
